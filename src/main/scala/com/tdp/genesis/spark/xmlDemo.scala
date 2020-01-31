9ad479de9a0e48728bcee5c671baa8e666c7e90e
danieledu <dportuga@everis.com>
2020-01-31 15:55:08 -0500

    spark config por parametros

--- src/main/scala/com/tdp/genesis/spark/xmlDemo.scala
+++ src/main/scala/com/tdp/genesis/spark/xmlDemo.scala
@@ -4,6 +4,7 @@ package com.tdp.genesis.spark
 import java.text.SimpleDateFormat
 import java.util.Date
 import org.apache.spark.sql.SparkSession
+import com.databricks.spark.xml._
 
 
 object xmlDemo {
@@ -16,40 +17,51 @@ object xmlDemo {
     val sdf = new SimpleDateFormat(dateFmt)
     sdf.format(date)
   }
+  println(
+    s"""
+       |
+       | ====================================================================
+       | =  Starting   SPARK                                                =
+       | =  Proceso:         XML_Demo                                       =
+       | =  Log Level:       ON                                            =
+       | =  Fecha de Inicio: ${today()}                         =
+       | ====================================================================
+       |
+       |""".stripMargin)
 
 
   def main(args: Array[String]): Unit = {
 
-    val master = args(0)
-
+    val master  = args(0)
+    val xmlFile = args(1)
+    val rowTag  = args(2)
+    val executorMemory  = args(3)
+    val driverMemory    = args(4)
+    val memoryOffHeap   = args(5)
 
     val spark = SparkSession.builder
       .appName("SparkSessionExample")
       .master(master)
-      .config("spark.executor.memory", "10g")
-      .config("spark.driver.memory", "5g")
+      .config("spark.executor.memory", executorMemory)
+      .config("spark.driver.memory", driverMemory)
       .config("spark.memory.offHeap.enabled", true)
-      .config("spark.memory.offHeap.size", "5g")
-      .enableHiveSupport()
+      .config("spark.memory.offHeap.size", memoryOffHeap)
       .getOrCreate
-
-
 
 
     //////////////////////////////////////////////////////////////////////////
 
+    val df = spark.read
+      .format("com.databricks.spark.xml")
+      .option("rowTag", rowTag)
+      .load(xmlFile)
 
-    println(
-      s"""
-         |
-         | ====================================================================
-         | =  Starting   SPARK                                                =
-         | =  Proceso:         XML_Demo                                       =
-         | =  Log Level:       OFF                                            =
-         | =  Fecha de Inicio: ${today()}                         =
-         | ====================================================================
-         |
-         |""".stripMargin)
+    println("--------------")
+    df.show(10 )
+    df.printSchema()
+
+
+
 
   }
 
